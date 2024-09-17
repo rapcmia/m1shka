b65964c762015b3866718766d4c6a2e7c507efba
Ralph Comia <rapcmia@mishka.local>
2024-09-17 11:14:11 +0800

    delted and modified hummingbot-cloner then add more to todo.py

--- scripts/python/todo.py
+++ scripts/python/todo.py
@@ -1,27 +1,43 @@
 print("\n******* TODO LIST *******\n")
 print("A simple python script for todo list\n")
 
+todo_list = ['','Show list','Add to list','Update list','Remove from list']
+
 def show_todo():
-    todo_list = ['','Show list','Add to list','Update list','Remove from list']
+    # displays the todo list
+
     for a in range(1, len(todo_list)):
         print(f"[{a}] {todo_list[a]}")
     print("\n")
     return
 
 def main():
-    try:
+    try:    
         while True:
             show_todo()
-            user_input = int(input("Enter a number for todo >> "))       
+            user_input = int(input("Enter a number for todo >> "))    
 
+            if user_input == 1:
+                pass
+            elif user_input == 2:
+                pass
+            elif user_input == 3:
+                pass
+            elif user_input == 4:
+                pass
+            else:
+                print("Please only select from the todo list\n")
+                return
+                            
             """
             Todo:
                 - execute the selected todo task
             """
 
             print("\n***\n")                  
-    except:
-        print("Please select from the todo list\n")
+    except ValueError:  # Specifically handle invalid integer input
+        print("Please only select from the todo list\n")
+
 
 
 if __name__ == "__main__":
